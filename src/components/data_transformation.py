cddfdf1252b1409078e95c07653b0e4bdfbc8690
Solidx74 <kareebsadab@gmail.com>
2026-03-03 07:19:07 +0600

    Data Transformation Completed

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -100,9 +100,10 @@ class DataTransformation:
             input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
 
-            train_arr = np.c_[input_feature_train_arr, target_feature_train_df.to_numpy()]
-            test_arr = np.c_[input_feature_test_arr, target_feature_test_df.to_numpy()]
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
+            logging.info("Saved preprocessing object on artifacts folder")
             # Save preprocessor object
             save_object(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
